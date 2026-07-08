09e4582bb4916eab16650126428767e4c35c5ebc
pranayh916 <pranayh916@gmail.com>
2026-07-09 00:55:10 +0530

    Add files via upload
    Third Day

---
+++ ThirdDay/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Student
+{
+   int age=20;
+   string name="Pranay Hajre";
+   void show()
+    {
+        Console.WriteLine("Name is: "+name);
+        Console.WriteLine("Age is: "+age);
+        Console.WriteLine("College name :  SSGMCE");
+        Console.WriteLine("Brance name:   CSE");
+         Console.WriteLine("Roll number:   63");
+
+
+
+    }
+   static void Main()
+    { 
+    // Student s=new Student();
+    // s.show();
+    // Array a=new Array();
+    // a.Display_List();
+    Monthly_Sales m=new Monthly_Sales();
+    m.show_Sales();
+    Console.WriteLine("----------------------------------------------------------------------------------------");
+    Library l=new Library();
+    l.show_books();
+
+        
+    } 
+}    
